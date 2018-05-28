2f93e5e9e574a3299deb67a2c4828606a58bfed1
Tezika Zhou <tezikazhou@gmail.com>
2018-05-28 08:40:29 +0000

    add the todos initially

--- public/app.js
+++ public/app.js
@@ -1,1 +1,20 @@
-alert("Hi from the app.js!");
+/* global $ */
+$(document).ready(function() {
+    $.getJSON('/api/todo')
+        .then(function(todos) {
+            todos.forEach(function(todo) {
+                addTodo(todo);
+            });
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+});
+
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    if (todo.completed) {
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+}
