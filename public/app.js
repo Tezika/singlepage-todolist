79096abe552b46be1f35853670380cd5cd116ffe
Tezika Zhou <tezikazhou@gmail.com>
2018-05-28 10:11:45 +0000

    finish the sweet todo single-page app

--- public/app.js
+++ public/app.js
@@ -9,12 +9,76 @@ $(document).ready(function() {
         .catch(function(err) {
             console.log(err);
         });
+
+    $('#todoInput').keypress(function(event) {
+        if (event.which == 13) {
+            createTodo();
+        }
+    });
+
+    $('.list').on('click', 'span', function(e) {
+        e.stopPropagation();
+        removeTodo($(this).parent());
+    });
+
+    $('.list').on('click', 'li', function() {
+        updateTodo($(this));
+    });
 });
 
+function removeTodo(todo) {
+    var clickedId = todo.data('id');
+    var deletedUrl = '/api/todo/' + clickedId;
+    $.ajax({
+            method: 'DELETE',
+            url: deletedUrl
+        })
+        .then(function(data) {
+            todo.remove();
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
+function createTodo() {
+    var userInput = $('#todoInput').val();
+    //send request to create new todo
+    $.post('/api/todo', { name: userInput })
+        .then(function(newTodo) {
+            addTodo(newTodo);
+            $('#todoInput').val('');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+}
+
 function addTodo(todo) {
-    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if (todo.completed) {
         newTodo.addClass('done');
     }
     $('.list').append(newTodo);
+}
+
+function updateTodo(todo) {
+    var isDone = !todo.data('completed');
+    var clickedId = todo.data('id');
+    var upadteData = { completed: isDone };
+    var updateUrl = '/api/todo/' + clickedId;
+    $.ajax({
+            method: 'PUT',
+            url: updateUrl,
+            data: upadteData
+        })
+        .then(function() {
+            todo.toggleClass("done");
+            todo.data('completed', isDone);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
